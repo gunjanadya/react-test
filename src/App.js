cccdc6a3e78ddec6754183d56d3644a090455778
gunjanadya <68297261+gunjanadya@users.noreply.github.com>
2024-03-03 13:52:25 -0500

    create ToDo.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ToDoList from './ToDoList';
 
 function App() {
+  const [todos, setTodos] = useState([{id : 1, name : 'todo 1', complete : false}])
+
   return (
-    <ToDoList />
+    <>
+      <ToDoList todos = {todos} />
+      <input type="text" />
+      <button>Add Tasks</button>
+      <button>Clear Completed Tasks</button>
+      <div>0 Tasks Remaining</div>
+    </>
+
   );
 }
 
---
+++ src/ToDo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ToDo([ todo ]) {
+    return (
+        <div>
+            <label>
+                {todo.name}
+            </label>
+
+        </div>
+    )
+}
